293ec7a2f084de654c0a99eb16a916458022fb03
PolarDyth <williamalexander1603@gmail.com>
2025-04-23 07:23:44 +0100

    feat: add @vercel/speed-insights package and integrate SpeedInsights component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +24,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={cn("min-h-screen bg-background antialiased", inter.className)}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
